8365bc8fdb5e011bd04649e04e21e4cc744f1f37
Natalia Homusko <avgust_net85@mail.ru>
2023-07-17 19:18:19 +0300

    fix URL

--- src/api/const.js
+++ src/api/const.js
@@ -1,3 +1,3 @@
 export const API_URI = 'https://dent-colorful-cyclone.glitch.me';
 
-export const URL_API_WS = 'ws://dent-colorful-cyclone.glitch.me/currency-feed';
+export const URL_API_WS = 'wss://dent-colorful-cyclone.glitch.me/currency-feed';
